6e3b9613fe2eae209f728eb0fbd079d8d1731d56
Jason Lee <jason.lee@cern.ch>
2016-04-05 04:03:02 +0900

    sanity check

--- L1TMuon/plugins/HitRateAnalysis.cc
+++ L1TMuon/plugins/HitRateAnalysis.cc
@@ -55,19 +55,26 @@ private:
   edm::InputTag gemPadDigiInput_;
   edm::InputTag gemCoPadDigiInput_;
 
+  typedef pair<GEMDetId,int> gemstrip;
+  vector<gemstrip> m_gemstrips;
+  
   edm::Service<TFileService> fs;
 
+  TH1F* h_stripsPerNBX;
   TH1F* h_digiPerNBX;
   TH1F* h_padPerNBX;
   TH1F* h_coPadPerNBX;
   int m_bxnumber;
-  const int m_maxbx = 40;
-  
-  int m_digiPerNBX[73][2][10][192];
-  int m_padPerNBX[73][2][10][192];
-  int m_coPadPerNBX[73][2][10][192];
+  long m_nevents;
+  const int m_maxbx = 5;
 
-  TH1F* h_nchamber;
+  int m_digiPerNBX[2][2][36][10][384];
+  int m_padPerNBX[2][2][36][10][384];
+  int m_coPadPerNBX[2][2][36][10][384];
+
+  TH1F* h_nstrips;
+  TH1F* h_npads;
+  TH1F* h_ncopads;
   
 };
 HitRateAnalysis::HitRateAnalysis(const edm::ParameterSet& iConfig)
@@ -77,26 +84,50 @@ HitRateAnalysis::HitRateAnalysis(const edm::ParameterSet& iConfig)
   gemCoPadDigiInput_ = iConfig.getParameter<edm::InputTag>("gemCoPadDigiInput");
 
   m_bxnumber = 0;
-  
-  h_nchamber=fs->make<TH1F>("nchamber","nchamber",73,0,73);
-  h_nchamber->GetXaxis()->SetTitle(Form("same digi hits within %2i BX", m_maxbx));
-  h_nchamber->GetYaxis()->SetTitle("Fraction");
+  m_gemstrips.clear();
+  for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j)
+      for (int c = 0; c < 36; ++c)
+	for (int f = 0; f < 10; ++f)
+	  for (int k = 0; k < 384; ++k){
+	    m_digiPerNBX[i][j][c][f][k] = 0;
+	    m_padPerNBX[i][j][c][f][k] = 0;
+	    m_coPadPerNBX[i][j][c][f][k] = 0;
+	  }
 
+  h_nstrips=fs->make<TH1F>("nstrips","nstrips",500,0,500);
+  h_nstrips->GetYaxis()->SetTitle("Fraction");
+
+  h_npads=fs->make<TH1F>("npads","npads",500,0,500);
+  h_npads->GetYaxis()->SetTitle("Fraction");
+
+  h_ncopads=fs->make<TH1F>("ncopads","ncopads",500,0,500);
+  h_ncopads->GetYaxis()->SetTitle("Fraction");
+  
   h_digiPerNBX=fs->make<TH1F>("digiPerNBX","digiPerNBX",10,0,10);
-  h_digiPerNBX->GetXaxis()->SetTitle(Form("same digi hits within %2i BX", m_maxbx));
+  h_digiPerNBX->GetXaxis()->SetTitle(Form("same digi hits within %2i events", m_maxbx));
   h_digiPerNBX->GetYaxis()->SetTitle("Fraction");
+
+  h_stripsPerNBX=fs->make<TH1F>("stripsPerNBX","stripsPerNBX",10,0,10);
+  h_stripsPerNBX->GetXaxis()->SetTitle(Form("same strips hits within %2i events", m_maxbx));
+  h_stripsPerNBX->GetYaxis()->SetTitle("Fraction");
   
   h_padPerNBX=fs->make<TH1F>("padPerNBX","padPerNBX",10,0,10);
-  h_padPerNBX->GetXaxis()->SetTitle(Form("same pad hits within %2i BX", m_maxbx));
+  h_padPerNBX->GetXaxis()->SetTitle(Form("same pad hits within %2i events", m_maxbx));
   h_padPerNBX->GetYaxis()->SetTitle("Fraction");
   
   h_coPadPerNBX=fs->make<TH1F>("coPadPerNBX","coPadPerNBX",10,0,10);
-  h_coPadPerNBX->GetXaxis()->SetTitle(Form("same coPad hits within %2i BX", m_maxbx));
+  h_coPadPerNBX->GetXaxis()->SetTitle(Form("same coPad hits within %2i events", m_maxbx));
   h_coPadPerNBX->GetYaxis()->SetTitle("Fraction");
-
+  m_nevents = 0;
 }
 HitRateAnalysis::~HitRateAnalysis()
 {
+  cout << "m_nevents = " << m_nevents << endl;
+  h_nstrips->Scale(1./m_nevents);
+  h_npads->Scale(1./m_nevents);
+  h_ncopads->Scale(1./m_nevents);
+  h_stripsPerNBX->Scale(1./h_digiPerNBX->Integral());
   h_digiPerNBX->Scale(1./h_digiPerNBX->Integral());
   h_padPerNBX->Scale(1./h_padPerNBX->Integral());
   h_coPadPerNBX->Scale(1./h_coPadPerNBX->Integral());
@@ -110,25 +141,29 @@ HitRateAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
   iEvent.getByLabel(gemDigiInput_, gem_digis);
   iEvent.getByLabel(gemPadDigiInput_, gemcscpad_digis);
   iEvent.getByLabel(gemCoPadDigiInput_, gemcsccopad_digis);
-
-  if (m_bxnumber == 0){
-    for (int i = 0; i < 73; ++i)
-      for (int j = 0; j < 2; ++j)
-	for (int f = 0; f < 10; ++f)
-	  for (int k = 0; k < 192; ++k){
-	    m_digiPerNBX[i][j][f][k] = 0;
-	    m_padPerNBX[i][j][f][k] = 0;
-	    m_coPadPerNBX[i][j][f][k] = 0;
-	  }
-  }
-  
+  m_nevents++;
+    
   for(GEMDigiCollection::DigiRangeIterator cItr = gem_digis->begin(); cItr != gem_digis->end(); ++cItr){
     GEMDetId id = (*cItr).first; 
     auto range((*cItr).second);
     for (GEMDigiCollection::const_iterator digiIt = range.first; digiIt != range.second; digiIt++) {
       if (id.station() == 1){
-	m_digiPerNBX[36+id.region()*id.chamber()][id.layer()-1][id.roll()-1][digiIt->strip()]++;
-	h_nchamber->Fill(id.roll()-1);
+	h_nstrips->Fill(digiIt->strip()-1);
+	m_gemstrips.push_back(make_pair(id, digiIt->strip()-1));
+	int region = 0;
+	if (id.region() > 0) region = 1;
+	m_digiPerNBX[region][id.layer()-1][id.chamber()-1][id.roll()-1][digiIt->strip()-1]++;
+	if (36+id.region()*id.chamber() < 0) cout <<"error 1"<<endl;
+	if (36+id.region()*id.chamber() >= 73) cout <<"error 2"<<endl;
+	if (36+id.region()*id.chamber() == 36) cout <<"error 3"<<endl;
+	if (id.layer()-1 < 0) cout <<"error 4"<<endl;
+	if (id.layer()-1 >= 2) cout <<"error 5"<<endl;
+	if (id.roll()-1 < 0) cout <<"error 6"<<endl;
+	if (id.roll()-1 >= 10) cout <<"error 7"<<endl;
+	if (digiIt->strip()-1 < 0) cout <<"error 8"<<endl;
+	if (digiIt->strip()-1 >= 384){
+	  cout <<"error 9 "<< digiIt->strip() <<endl;
+	}
       }
     }
   }
@@ -137,8 +172,15 @@ HitRateAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
     GEMDetId id = (*cItr).first; 
     auto range((*cItr).second);
     for (GEMCSCPadDigiCollection::const_iterator digiIt = range.first; digiIt != range.second; digiIt++) {
-      if (id.station() == 1){	
-	m_padPerNBX[36+id.region()*id.chamber()][id.layer()-1][id.roll()-1][digiIt->pad()]++;
+      if (id.station() == 1){
+	h_npads->Fill(digiIt->pad()-1);
+	int region = 0;
+	if (id.region() > 0) region = 1;
+	m_padPerNBX[region][id.layer()-1][id.chamber()-1][id.roll()-1][digiIt->pad()-1]++;
+	if (digiIt->pad()-1 < 0) cout <<"error 10"<<endl;
+	if (digiIt->pad()-1 >= 192){
+	  cout <<"error 11 "<< digiIt->pad() <<endl;
+	}	
       }
     }
   }
@@ -148,7 +190,14 @@ HitRateAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
     auto range((*cItr).second);
     for (GEMCSCCoPadDigiCollection::const_iterator digiIt = range.first; digiIt != range.second; digiIt++) {
       if (id.station() == 1){
-	m_coPadPerNBX[36+id.region()*id.chamber()][id.layer()-1][id.roll()-1][digiIt->first().pad()]++;
+	h_ncopads->Fill(digiIt->first().pad()-1);
+	int region = 0;
+	if (id.region() > 0) region = 1;
+	m_coPadPerNBX[region][id.layer()-1][id.chamber()-1][id.roll()-1][digiIt->first().pad()-1]++;
+	if (digiIt->first().pad()-1 < 0) cout <<"error 12"<<endl;
+	if (digiIt->first().pad()-1 >= 192){
+	  cout <<"error 13 "<< digiIt->first().pad() <<endl;
+	}
 	//cout <<"id.layer() " << id.layer()-1 << endl;
       }
     }
@@ -156,17 +205,47 @@ HitRateAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
   m_bxnumber++;
   
   if (m_bxnumber == m_maxbx){
-    for (int i = 0; i < 73; ++i)
-      if (i != 36)
-	for (int j = 0; j < 2; ++j)
+    for (int i = 0; i < 2; ++i)
+      for (int j = 0; j < 2; ++j)
+	for (int c = 0; c < 36; ++c)
 	  for (int f = 0; f < 10; ++f)
-	    for (int k = 0; k < 192; ++k){
-	      h_digiPerNBX->Fill(m_digiPerNBX[i][j][f][k]);
-	      h_padPerNBX->Fill(m_padPerNBX[i][j][f][k]);
-	      if (j==0)
-		h_coPadPerNBX->Fill(m_coPadPerNBX[i][j][f][k]);
+	    for (int k = 0; k < 384; ++k){
+	      int region = -1;
+	      if (i == 1) region = 1;
+	      GEMDetId id = GEMDetId(region, 1,1,j+1,c+1,f+1);
+	      int nhits = 0;
+	      for (unsigned int ns = 0; ns < m_gemstrips.size() ; ns++){
+		if (m_gemstrips[ns].first == id){
+		  if (m_gemstrips[ns].second == k){
+		    nhits++;
+		  }
+		}
+	      }
+	      h_stripsPerNBX->Fill(nhits);
+
+	      h_digiPerNBX->Fill(m_digiPerNBX[i][j][c][f][k]);
+
+	      if (k < 192){
+		h_padPerNBX->Fill(m_padPerNBX[i][j][c][f][k]);
+		if (j == 0){
+		  h_coPadPerNBX->Fill(m_coPadPerNBX[i][j][c][f][k]);
+		}
+	      }
 	    }
+
+    // reset all
     m_bxnumber = 0;
+    m_gemstrips.clear();
+    for (int i = 0; i < 2; ++i)
+      for (int j = 0; j < 2; ++j)
+	for (int c = 0; c < 36; ++c)
+	  for (int f = 0; f < 10; ++f)
+	    for (int k = 0; k < 384; ++k){
+	      m_digiPerNBX[i][j][c][f][k] = 0;
+	      m_padPerNBX[i][j][c][f][k] = 0;
+	      m_coPadPerNBX[i][j][c][f][k] = 0;
+	    }
+    
   }
   
 }
--- L1TMuon/test/runHitRateAnalysis.py
+++ L1TMuon/test/runHitRateAnalysis.py
@@ -1,13 +1,21 @@
-runevents = 1000
-runevents = -1;
+runevents = 2000
+#runevents = -1;
 
 import FWCore.ParameterSet.Config as cms
 process = cms.Process('HitRateAnalysis')
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
-fileOutputName = "test"
-histofileName= "histo_"+fileOutputName+".root"
+pu = '200'
+pu = '140'
+
+if pu == '200':
+    datadir = '/pnfs/user/jlee/data/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/TP2023HGCALDR-HGCALForMUO_PU200BX25_newsplit_PH2_1K_FB_V6-v1/GEN-SIM-DIGI-RAW/'
+
+if pu == '140':
+    datadir = '/pnfs/user/jlee/data/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/TP2023HGCALDR-HGCALMar26_PU140BX25_PH2_1K_FB_V6-v2/GEN-SIM-DIGI-RAW/'
+    
+histofileName= "histo_"+pu+".root"
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(runevents)
@@ -16,11 +24,8 @@ process.maxEvents = cms.untracked.PSet(
 # Input source
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
 import os
-datadir = '/pnfs/user/jlee/data/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/TP2023HGCALDR-HGCALMar26_PU140BX25_PH2_1K_FB_V6-v2/GEN-SIM-DIGI-RAW/'
-#datadir = '/pnfs/user/jlee/data/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/TP2023HGCALDR-HGCALForMUO_PU200BX25_newsplit_PH2_1K_FB_V6-v1/GEN-SIM-DIGI-RAW/'
+
 for f in os.listdir(datadir):
-    if '50983EA5-1FE2-E411-91B1-008CFA010D18.root' in f:
-        continue
     if '.root' in f:
         process.source.fileNames.append("file:"+datadir+f)
 
